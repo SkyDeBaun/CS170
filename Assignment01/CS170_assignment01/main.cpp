690fdab945818b688465b1a8e4adf584a950a67d
SkyDeBaun <43687571+SkyDeBaun@users.noreply.github.com>
2020-05-01 18:24:25 -0700

    project setup - initial commit
    add main skeleton, config .gitignore

---
+++ Assignment01/CS170_assignment01/main.cpp
@@ -0,0 +1,21 @@
+//Sky DeBaun -> CS170 -> Intro to Artificial Intelligence -> Project 01
+//Professor Niloofar Montazeri -> Spring 2020
+//A* -> eight-puzzle
+
+
+
+#include <iostream>
+
+
+
+int main()
+{
+
+
+
+	return 0;
+}
+
+
+
+
