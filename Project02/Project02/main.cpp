7f714e51498f302ca4b47ab2b1db466b58ebc2c9
SkyDeBaun <43687571+SkyDeBaun@users.noreply.github.com>
2020-05-22 23:41:49 -0700

    Reading sample data
    Converting each scientific notation into string, verified via cout to console.
    -next will be converting to doubles (and putting in vector).. though that may be a bit ineficient.
    *may just return double as file is parsed

--- Project02/Project02/main.cpp
+++ Project02/Project02/main.cpp
@@ -1,11 +1,123 @@
+//CS170 Project02
 
+//includes--------------------------------
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
+//using-----------------------------------
+using std::vector;
+using std::fstream;
+using std::string;
+using std::ios;
+using std::cout;
 
+//function prototypes----------------------
+bool openFile(fstream &file, const char *name);
+void convertFile(fstream &file, const char *name, vector<double> &numbers);
+
+//MAIN------------------------------------------------
+//----------------------------------------------------
 int main()
 {
+	vector <double> dataTest;
+
+	fstream file;
+	if (openFile(file, "cs_170_small80.txt"))
+	{
+		string word;
+		char ch;
+		int counter = 0;
+		bool newWord = false;
+
+		file.get(ch);
+
+		while (!file.eof())
+		{
+			
+			if (ch != ' ' && ch != 10)//if not blank or newline
+			{
+				word.push_back(ch);
+				newWord = true;//on a new word
+			}
+			else if ((ch == ' '|| ch == 10) && newWord)
+			{				
+					cout << word << std::endl;
+					word.clear();
+					newWord = false;// ready for next new word
+								
+			}
+
+			file.get(ch); //get next char from file
+		}
+	}
+	else
+	{
+		cout << "Error reading file \n";
+	}
 
 
 
 	return 0;
-}
+
+}//end main-------------------------------------------///
+
+
+
+
+bool openFile(fstream &file, const char *name)
+{
+	bool status;
+
+	file.open(name, ios::in);
+	if (file.fail())
+	{
+		status = false;
+	}
+	else
+	{
+		status = true;
+	}
+
+	return status;
+}//end readFile---//
+
+
+void convertFile(fstream &file, const char *name, vector<double> &numbers)
+{
+	if (openFile(file, name))
+	{
+		string word;
+		char ch;
+		int counter = 0;
+		bool newWord = false;
+
+		file.get(ch);
+
+		while (!file.eof())
+		{
+
+			if (ch != ' ' && ch != 10)//if not blank or newline
+			{
+				word.push_back(ch);
+				newWord = true;//on a new word
+			}
+			else if ((ch == ' ' || ch == 10) && newWord)
+			{
+				cout << word << std::endl;
+				word.clear();
+				newWord = false;// ready for next new word
+
+			}
+
+			file.get(ch); //get next char from file
+		}
+	}
+	else
+	{
+		cout << "Error reading file \n";
+	}
+
+}//end convertFile---//
+
