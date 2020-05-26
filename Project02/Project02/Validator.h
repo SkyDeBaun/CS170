d1ae252e4740c75966a7603512bc1781f1fefbaf
SkyDeBaun <43687571+SkyDeBaun@users.noreply.github.com>
2020-05-26 03:21:58 -0700

    finished earlier comment descritpion

--- Project02/Project02/Validator.h
+++ Project02/Project02/Validator.h
@@ -1,6 +1,6 @@
 #pragma once
 //CS170 -> Project02 -> Sky DeBaun
-//leave-one-out validator uses training data and classifier to find and return accuracy.
+//leave-one-out validator uses training data and classifier to find and return accuracy of subset of features
 
 #include "DataObject.h"
 #include "NearestNeighborClassifier.h"
