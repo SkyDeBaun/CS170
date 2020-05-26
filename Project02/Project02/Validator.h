39506fb2a53fad6f8bec2f49e103c5fa2c047264
SkyDeBaun <43687571+SkyDeBaun@users.noreply.github.com>
2020-05-26 03:10:48 -0700

    validator comments added

--- Project02/Project02/Validator.h
+++ Project02/Project02/Validator.h
@@ -1,4 +1,9 @@
 #pragma once
+//CS170 -> Project02 -> Sky DeBaun
+//leave-one-out validator uses training data and classifier to find and return accuracy.
+
+#include "DataObject.h"
+#include "NearestNeighborClassifier.h"
 
 
 class Validator
