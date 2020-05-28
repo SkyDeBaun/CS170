12eb5cbfd0d94b85bc2e4065847a9cd04c208a9e
SkyDeBaun <43687571+SkyDeBaun@users.noreply.github.com>
2020-05-28 00:11:01 -0700

    minor edits: clean up

--- Project02/Project02/main.cpp
+++ Project02/Project02/main.cpp
@@ -34,32 +34,25 @@ using std::cout;
 int main()
 {
 	//convert sample data set to numeric values-------
-	DataParser conversion; //data conversion object parses ascii-text datafile and stores its normalized dataset
 
 	//small data set----------------------------------
 	cout << "Small Dataset: ";
-	vector <double> smallDataVector;
-	DataObject *data;
-	data = new DataObject(smallDataVector);
+	vector <double> dataVector;
 
+	//prepare data object (holds vector and normalization routine)
+	DataObject *data;
+	data = new DataObject(dataVector);
+
+	//data parser-------------------------------------
+	DataParser conversion; //data conversion object parses ascii-text datafile and stores its normalized dataset
 	conversion.parseDataFile("data/cs_170_small80.txt", data, true); //parameters: filename, vector, debug = false
 	conversion.printTable(data); //debug -> verify: normalized
 
+	//search------------------------------------------
 	Search searchFeatures(data);
 	searchFeatures.findBestFeatures();
 
 
-	//classifier initialization-----------------------
-	//NearestNeighborClassifier classifier(smallData);
-	//NearestNeighborClassifier *classifier;
-	//classifier = new NearestNeighborClassifier(data);
-
-	//vector<bool> key(11, true);//test key
-	//vector<bool> key{false,false, true, false, true, false, false, false, false, false, false };//test key
-	//vector<bool> key{ false, false,false, true, false, true, false, false, false, false, false };//test key 91%
-
-	//Validator validatator(classifier);//test
-	// << "Accuracy: " << validatator.validate(key) << "\n";//test
 
 	//large data set----------------------------------
 	cout<< "\n" << "Large Dataset: ";
